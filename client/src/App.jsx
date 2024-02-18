a4ef0b9eb77339707e633d70b8322ab9467ba69a
Mayank <mayank0real0world@gmail.com>
2024-02-18 16:29:37 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-[#000000] h-[900px]'>
+      
+    <h1 className='text-white align'>
+      snvso
+    </h1>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
